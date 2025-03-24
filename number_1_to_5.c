e0242a5945092dfccdd3febd8d8fce5f7b07fe69
brent just brent <belmontebrent@gmail.com>
2025-03-24 20:02:57 +0800

    number_1_to_5.c

---
+++ number_1_to_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    // Loop to print numbers from 1 to 100
+    for (i = 1; i <= 5; i++) {
+        printf("%d\n", i);  // Prints the current value of i
+    }
+
+    printf("\n");  // Moves to the next line after printing all numbers
+    return 0;
+}
